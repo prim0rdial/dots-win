34ee666c8270961db01d04359d4bbac0c45df372
prim0rdial <jeremy.pro.lp@gmail.com>
2021-02-13 13:24:00 +0100

    slight refactoring mostly clean up

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -1,5 +1,4 @@
-# Location ProgramFiles\PowerShell\7-preview
-
+# Colored ls output with Get-ChildItemColor
 If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
     Import-Module Get-ChildItemColor
 
@@ -7,16 +6,47 @@ If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not i
     Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
 }
 
-Function h() {
-   cd ~/
+
+# Helpers
+
+# Quick Access Most use directories
+function db {
+  set-location "C:\Users\jerem\Dropbox"
 }
 
-function xa { set-location C:\xampp }
-function ht { set-location C:\xampp\htdocs }
-function ho { set-location C:\user\jerem }
-function db { set-location C:\Users\jerem\Dropbox }
-function docs { set-location C:\Users\jerem\Documents }
-function touch {set-content -Path ($args[0]) -Value ($null)} 
+function dc {
+  set-location "C:\Users\jerem\Documents"
+}
+
+function dots {
+  set-location "C:\Users\jerem\Documents\dots-win"
+}
+
+Function ho {
+   set-location "C:\Users\jerem"
+}
+
+# Php / Mysql Locations
+function xa {
+  set-location "C:\xampp"
+}
+
+function ht {
+  set-location "C:\xampp\htdocs"
+}
+
+function pg {
+  set-location "C:\Prog"
+}
+
+function config {
+  code "C:\Program Files\PowerShell\7-preview"
+}
+
+# Touch like alias
+function touch {
+  set-content -Path ($args[0]) -Value ($null)
+}
 
 function install() {
   choco install -y $args
